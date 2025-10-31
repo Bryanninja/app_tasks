113b8b17821417a418c9675ca7b5858048424263
Álex Bryan <bryannascimentopl800@gmail.com>
2025-10-31 13:23:25 -0300

    chore: add tailwindcss setup

--- fsc-task-mananger/src/App.jsx
+++ fsc-task-mananger/src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div>
+      <h1 className="text-red-100">Ola Meu Pai</h1>
+    </div>
+  );
 }
 
 export default App
--- fsc-task-mananger/vite.config.js
+++ fsc-task-mananger/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    // Adicione ou ajuste esta seção
+    watch: {
+      usePolling: true, // Força o Vite a verificar ativamente as mudanças
+    },
+  },
+});
