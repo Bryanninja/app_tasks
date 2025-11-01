e5cdb51415f0b72bddd090dce3ff7d7f7190d18c
Álex Bryan <bryannascimentopl800@gmail.com>
2025-11-01 14:33:17 -0300

    chore: prettier otimization

--- fsc-task-mananger/src/Tasks.jsx
+++ fsc-task-mananger/src/Tasks.jsx
@@ -1,6 +1,8 @@
-function Tasks(){
-    return(
-        <h2>Ola</h2>
-    );
+function Tasks() {
+  return (
+    <h2 className="mt-2 bg-black px-4 py-2 font-bold text-red-400">
+      Olasdssada
+    </h2>
+  );
 }
 export default Tasks;
