3d8506c8673193fc5b0c7df7f7a07c96ad8bbe9e
Álex Bryan <bryannascimentopl800@gmail.com>
2025-11-04 11:16:37 -0300

    feat: ensure toast is show after task update

--- fsc-task-mananger/src/components/Tasks.jsx
+++ fsc-task-mananger/src/components/Tasks.jsx
@@ -8,6 +8,7 @@ import TasksSeparator from './TasksSeparator';
 import TASKS from '../constants/tasks';
 import { useState } from 'react';
 import TaskItem from './TaskItem';
+import { toast } from 'sonner';
 
 function Tasks() {
   const [tasks, setTasks] = useState(TASKS);
@@ -19,6 +20,7 @@ function Tasks() {
   const handleTaskDeleteClick = (taskId) => {
     const newTasks = tasks.filter((task) => task.id != taskId);
     setTasks(newTasks);
+    toast.success('Tarefa deletada com sucesso!');
   };
 
   const handleTaskCheckboxClick = (taskId) => {
@@ -28,14 +30,17 @@ function Tasks() {
       }
 
       if (task.status == 'not_started') {
+        toast.success('tarefa iniciada com sucesso!');
         return { ...task, status: 'in_progress' };
       }
 
       if (task.status == 'in_progress') {
+        toast.success('tarefa concluída com sucesso!');
         return { ...task, status: 'done' };
       }
 
       if (task.status == 'done') {
+        toast.success('tarefa reiniciada com sucesso!');
         return { ...task, status: 'not_started' };
       }
 
