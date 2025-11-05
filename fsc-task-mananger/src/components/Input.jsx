4dbfd40e6860eb6be51420867b6af30dc53c2994
Álex Bryan <bryannascimentopl800@gmail.com>
2025-11-05 11:20:34 -0300

    feat: add inputLabel component

--- fsc-task-mananger/src/components/Input.jsx
+++ fsc-task-mananger/src/components/Input.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
+import InputLabel from './InputLabel';
+
 export default function Input({ label, ...rest }) {
   return (
     <div className="flex flex-col space-y-1 text-left">
-      <label className="text-sm font-semibold text-[#35383E]" htmlfor={rest.id}>
-        {label}
-      </label>
+      <InputLabel htmlFor={rest.id}>{label}</InputLabel>
       <input
         className="rounded-lg border border-solid border-[#ECECEC] px-4 py-3 outline-[#00ADB5] placeholder:text-sm placeholder:text-[#9A9C9F]"
         {...rest}
---
+++ fsc-task-mananger/src/components/InputLabel.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function InputLabel(props) {
+  return (
+    <label className="text-sm font-semibold text-[#35383E]" {...props}>
+      {props.children}
+    </label>
+  );
+}
