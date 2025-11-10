e7a913696e1365523f34d24d57da2fa810a3b097
Álex Bryan <bryannascimentopl800@gmail.com>
2025-11-10 16:38:06 -0300

    fix: errorMessage and option Selected fixed

--- fsc-task-mananger/src/components/TimeSelect.jsx
+++ fsc-task-mananger/src/components/TimeSelect.jsx
@@ -1,17 +1,21 @@
+// TimeSelect.jsx
+
 import React, { forwardRef } from 'react';
 
 import InputErrorMessage from './InputErrorMessage';
 import InputLabel from './InputLabel';
 
-const TimeSelect = forwardRef((props, ref) => {
+// Desestruture errorMessage do restante das props
+const TimeSelect = forwardRef(({ errorMessage, ...rest }, ref) => {
   return (
     <div className="flex flex-col space-y-1 text-left">
       <InputLabel htmlFor="time">Horário</InputLabel>
 
       <select
         id="time"
-        className="outline-brand-primary placeholder:text-brand-text-gray border-brand-border rounded-lg border border-solid px-4 py-3 placeholder:text-sm"
-        {...props}
+        className="rounded-lg border border-solid border-brand-border px-4 py-3 outline-brand-primary placeholder:text-sm placeholder:text-brand-text-gray"
+        // Espalhe apenas as props restantes (sem errorMessage)
+        {...rest}
         ref={ref}
       >
         <option value="" disabled selected>
@@ -22,9 +26,8 @@ const TimeSelect = forwardRef((props, ref) => {
         <option value="evening">Noite</option>
       </select>
 
-      {props.errorMessage && (
-        <InputErrorMessage>{props.errorMessage}</InputErrorMessage>
-      )}
+      {/* Use errorMessage aqui, onde é relevante para a mensagem de erro */}
+      {errorMessage && <InputErrorMessage>{errorMessage}</InputErrorMessage>}
     </div>
   );
 });
