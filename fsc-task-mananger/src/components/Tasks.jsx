4d9ccfb7390f1874332ca3cd15d722902cbcd5f8
Álex Bryan <bryannascimentopl800@gmail.com>
2025-11-11 15:03:03 -0300

    fix: imports Error fixed

--- fsc-task-mananger/src/components/Tasks.jsx
+++ fsc-task-mananger/src/components/Tasks.jsx
@@ -8,7 +8,6 @@ import {
   SunIcon,
   TrashIcon,
 } from '../assets/icons';
-import TASKS from '../constants/tasks';
 import AddTaskDialog from './AddTaskDialog';
 import Button from './Button';
 import TaskItem from './TaskItem';
