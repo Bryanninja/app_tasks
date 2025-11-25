1a76ffd2df8319ae7816ae9fdbee9bef79e82d9a
Álex Bryan <bryannascimentopl800@gmail.com>
2025-11-25 15:33:37 -0300

    fix: ensure add tasks can use API

--- src/hooks/data/use-add-task.js
+++ src/hooks/data/use-add-task.js
@@ -1,18 +1,15 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import axios from 'axios';
 
 import { taskMutationKeys } from '../../keys/mutations';
 import { taskQueryKeys } from '../../keys/queries';
+import { api } from '../../lib/axios';
 
 export const useAddTask = () => {
   const queryClient = useQueryClient();
   return useMutation({
     mutationKey: taskMutationKeys.add(),
     mutationFn: async (task) => {
-      const { data: createdTask } = await axios.post(
-        'http://localhost:3000/tasks',
-        task
-      );
+      const { data: createdTask } = await api.post(`/tasks/${task}`);
       return createdTask;
     },
     onSuccess: (createdTask) => {
